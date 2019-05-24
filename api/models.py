8a950289bc21a4e9f3f79b234e5a791a1cbaf8bc
tamercuba <tamercuba@gmail.com>
2019-05-24 12:18:05 -0300

    erro de digitacao em NUMERACAO_CHOICES corrigido

--- api/models.py
+++ api/models.py
@@ -21,15 +21,15 @@ class Calcado(models.Model):
     ]
 
     #    NUMERACOES
-    NUMERACAO_CHOCES = [(num, str(num)) for num in range(35,46,1)]
+    NUMERACAO_CHOICES = [(num, str(num)) for num in range(35,46,1)]
 
     #    CAMPOS
     _id         = models.AutoField(primary_key=True)
     descricao   = models.CharField(max_length=100, null=False, blank = False)
     fornecedor  = models.CharField(max_length=100, null=False, blank=False)
     tipo        = models.CharField(max_length=2, choices=TIPO_CHOICES, null=False, blank=False)
-    numeracao   = models.IntegerField(choices=NUMERACAO_CHOCES, null=False, blank=False)
-    quantidade  = models.IntegerField(null = False, blank = False, default = 0)
+    numeracao   = models.IntegerField(choices=NUMERACAO_CHOICES, null=False, blank=False)
+    quantidade  = models.IntegerField(null = False, default = 0)
     preco_custo = models.DecimalField(
         max_digits=6, decimal_places=2,
         verbose_name='Preco de Custo', null=False, blank=False

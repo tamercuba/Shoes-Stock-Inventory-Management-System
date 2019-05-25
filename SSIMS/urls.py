a00ebb270898f3f65fa2bf6ffc6e0ea4a02ef032
tamercuba <tamercuba@gmail.com>
2019-05-25 20:36:57 -0300

    urls adicionada

--- SSIMS/urls.py
+++ SSIMS/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from .routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('', include('api.urls')),
 ]

5379b8b60427d8ff75b6efb7f921d449ca7af0a7
tamercuba <tamercuba@gmail.com>
2019-05-25 20:43:07 -0300

    rest paginator alterado

--- SSIMS/settings.py
+++ SSIMS/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3
+    'PAGE_SIZE': 6
 }
 
 

14c98fbbf66d49d0bca443166cdcd0095f67498a
tamercuba <tamercuba@gmail.com>
2019-05-26 23:48:47 -0300

    metodo custom_create adicionado

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import Calcado
-from .serializers import CalcadoSerializer
+from .serializers import CalcadoSerializer, EstoqueSerializer, EstoquePostSerializer
 from .filters import CalcadoFilter
+from rest_framework.response import Response
+from rest_framework import status
 
 class CalcadoViewSet(viewsets.ModelViewSet):
     '''
@@ -29,3 +31,16 @@ class CalcadoViewSet(viewsets.ModelViewSet):
     queryset = Calcado.objects.all()
     serializer_class = CalcadoSerializer
     filter_class = CalcadoFilter
+
+    def custom_create(self, request, *args, **kwargs):
+        data = {}
+
+        data['tamanho'] = request.data.get('tamanho')
+        data['quantidade'] = request.data.get('quantidade')
+        data['id_calcado'] = kwargs['pk']
+
+        custom_serializer = EstoquePostSerializer(data=data)
+        custom_serializer.is_valid(raise_exception=True)
+        self.perform_create(custom_serializer)
+        headers = self.get_success_headers(custom_serializer.data)
+        return Response(custom_serializer.data, status=status.HTTP_201_CREATED, headers=headers)

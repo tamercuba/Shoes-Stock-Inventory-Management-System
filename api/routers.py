c1b158da41b411cc0b2975bc53f59157699144e0
tamercuba <tamercuba@gmail.com>
2019-05-28 17:23:24 -0300

    rotas irrelevantes removidas

--- api/routers.py
+++ api/routers.py
@@ -15,15 +15,6 @@ class CustomRouter(DefaultRouter):
             detail=False,
             initkwargs={'suffix': 'List'}
         ),
-        # Dynamically generated list routes. Generated using
-        # @action(detail=False) decorator on methods of the viewset.
-        DynamicRoute(
-            url=r'^{prefix}/{url_path}{trailing_slash}$',
-            name='{basename}-{url_name}',
-            detail=False,
-            initkwargs={}
-        ),
-        # Detail route.
         Route(
             url=r'^{prefix}/{lookup}{trailing_slash}$',
             mapping={
@@ -36,14 +27,6 @@ class CustomRouter(DefaultRouter):
             name='{basename}-detail',
             detail=True,
             initkwargs={'suffix': 'Instance'}
-        ),
-        # Dynamically generated detail routes. Generated using
-        # @action(detail=True) decorator on methods of the viewset.
-        DynamicRoute(
-            url=r'^{prefix}/{lookup}/{url_path}{trailing_slash}$',
-            name='{basename}-{url_name}',
-            detail=True,
-            initkwargs={}
         ),
     ]
 

dde4a7cb95e87f7c90b7631262d8a8ead7103216
tamercuba <tamercuba@gmail.com>
2019-05-29 11:43:22 -0300

    CustomRouter substituido por DefaultRouter

--- api/routers.py
+++ api/routers.py
@@ -1,34 +1,6 @@
 # from rest_framework import routers
 from .viewsets import CalcadoViewSet
-from rest_framework.routers import DefaultRouter, Route, DynamicRoute
+from rest_framework.routers import DefaultRouter
 
-class CustomRouter(DefaultRouter):
-    routes = [
-        # List route.
-        Route(
-            url=r'^{prefix}{trailing_slash}$',
-            mapping={
-                'get': 'list',
-                'post': 'create'
-            },
-            name='{basename}-list',
-            detail=False,
-            initkwargs={'suffix': 'List'}
-        ),
-        Route(
-            url=r'^{prefix}/{lookup}{trailing_slash}$',
-            mapping={
-                'get': 'retrieve',
-                'post': 'custom_create',
-                'put': 'update',
-                'patch': 'partial_update',
-                'delete': 'destroy'
-            },
-            name='{basename}-detail',
-            detail=True,
-            initkwargs={'suffix': 'Instance'}
-        ),
-    ]
-
-router = CustomRouter()
+router = DefaultRouter()
 router.register(r'resources', CalcadoViewSet)

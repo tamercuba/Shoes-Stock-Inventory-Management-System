770127377271570dc7ecb5a29be80fad4a53be30
tamercuba <tamercuba@gmail.com>
2019-05-29 12:37:34 -0300

    frontend removido do app api

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from .routers import router
-from . import views
 
 app_name = 'api'
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('', include(router.urls))
 ]

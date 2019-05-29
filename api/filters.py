b04bedf564e0eb72902e8f5be33095c9b63e5c9c
tamercuba <tamercuba@gmail.com>
2019-05-29 11:42:57 -0300

    indentacao corrigida

--- api/filters.py
+++ api/filters.py
@@ -12,11 +12,11 @@ class CalcadoFilter(filters.FilterSet):
     fornecedor__ll = filters.CharFilter(field_name='fornecedor', lookup_expr='icontains')
     descricao__ll  = filters.CharFilter(field_name='descricao', lookup_expr='icontains')
     class Meta:
-        model = Calcado
+        model  = Calcado
         fields = {
-            'descricao':['exact', 'll'],
-            'fornecedor': ['exact', 'll'],
-            'preco_custo': ['exact', 'lt', 'lte', 'gt', 'gte'],
-            'preco_venda': ['exact', 'lt', 'lte', 'gt', 'gte'],
-            'tipo': ['exact'],
+            'descricao':    ['exact', 'll'],
+            'fornecedor':   ['exact', 'll'],
+            'preco_custo':  ['exact', 'lt', 'lte', 'gt', 'gte'],
+            'preco_venda':  ['exact', 'lt', 'lte', 'gt', 'gte'],
+            'tipo':         ['exact'],
         }

188cd2e122e273abc8fd4fe11e7fe47f96eb7a2a
tamercuba <tamercuba@gmail.com>
2019-05-29 11:44:09 -0300

    metodo custom_create excluido

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from .models import Calcado, Estoque
-from .serializers import CalcadoSerializer, EstoqueSerializer, EstoquePostSerializer
+from .serializers import CalcadoSerializer, EstoqueSerializer
 from .filters import CalcadoFilter
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,9 +16,6 @@ class CalcadoViewSet(viewsets.ModelViewSet):
 
     create:
     Adiciona um novo calçado (Verbo CREATE)
-
-    custom_create:
-    Adiciona uma instancia ao estoque (Verbo CREATE)
 
     list:
     Retorna uma lista de todos os calçados (Verbo GET)
@@ -37,27 +34,5 @@ class CalcadoViewSet(viewsets.ModelViewSet):
     '''
 
     queryset = Calcado.objects.all()
+    serializer_class = CalcadoSerializer
     filter_class = CalcadoFilter
-
-    def get_serializer_class(self):
-        if self.action == 'custom_create':
-            return EstoquePostSerializer
-        else:
-            return CalcadoSerializer
-
-    def custom_create(self, request, *args, **kwargs):
-        data = {}
-
-        data['id_calcado'] = kwargs['pk']
-        data['tamanho'] = request.data.get('tamanho')
-        data['quantidade'] = request.data.get('quantidade')
-
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-
-        calcado_serializer = CalcadoSerializer(instance=self.get_object())
-        #calcado_serializer = CalcadoSerializer(data=instance)
-        # response_serializer.is_valid()
-        return Response(calcado_serializer.data, status=status.HTTP_201_CREATED, headers=headers)

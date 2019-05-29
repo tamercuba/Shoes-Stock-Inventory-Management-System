7ff911249c11a226411780857e4f8b1b89b2bedb
tamercuba <tamercuba@gmail.com>
2019-05-29 11:43:37 -0300

    metodo update customizado

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,12 @@
 from rest_framework import serializers
 from .models import Calcado, Estoque
 
+
 class EstoqueSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Estoque
         fields = ['tamanho', 'quantidade']
-
-class EstoquePostSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Estoque
-        fields = ['id_calcado','tamanho', 'quantidade']
 
 class CalcadoSerializer(serializers.ModelSerializer):
     '''
@@ -21,7 +16,37 @@ class CalcadoSerializer(serializers.ModelSerializer):
     modelo Django para dados tipo Python nativo,
     para depois serem convertidos em formato JSON.
     '''
-    estoque = EstoqueSerializer(many=True, read_only=True)
+    estoque = EstoqueSerializer(many=True)
+
+    def update(self, instance, validated_data):
+        instance.descricao   = validated_data.get(
+            'descricao', instance.descricao)
+        instance.fornecedor  = validated_data.get(
+            'fornecedor', instance.fornecedor)
+        instance.tipo        = validated_data.get('tipo', instance.tipo)
+        instance.preco_custo = validated_data.get(
+            'preco_custo', instance.preco_custo)
+        instance.preco_venda = validated_data.get(
+            'preco_venda', instance.preco_venda)
+
+        estoque      = instance.estoque.all()
+        estoque_data = validated_data.get('estoque', [])
+
+        for item_data in estoque_data:
+            item_id = item_data.get('tamanho', None)
+            if item_id is not None:
+                item_db            = estoque.get(tamanho=item_id)
+                item_db.tamanho    = item_data.get('tamanho', item_db.tamanho)
+                item_db.quantidade = item_data.get('quantidade',item_db.quantidade)
+                item_db.save()
+            else:
+                Estoque.objects.create(
+                    id_calcado = instance,
+                    tamanho    = item_data['tamanho'],
+                    quantidade = item_data['quantidade']
+                )
+        instance.save()
+        return instance
 
     class Meta:
         model = Calcado

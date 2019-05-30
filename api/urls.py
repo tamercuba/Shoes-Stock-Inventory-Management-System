f37e0b125fb86bb7cfd5ea95bee3455ae683de50
tamercuba <tamercuba@gmail.com>
2019-05-30 12:16:54 -0300

    url csv adicionada

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from .routers import router
+from .viewsets import FileUploadView
 
 app_name = 'api'
 
 urlpatterns = [
+    path('resources/csv_import/', FileUploadView.as_view()),
     path('', include(router.urls))
 ]
